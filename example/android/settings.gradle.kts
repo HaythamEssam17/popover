d5de62942966f312fccb2cc0b236059eb26e6527
haitham <45662826+HaythamEssam17@users.noreply.github.com>
2025-05-01 14:51:55 +0300

    running on android kts files

--- example/android/settings.gradle.kts
+++ example/android/settings.gradle.kts
@@ -1,12 +1,12 @@
-import java.util.Properties
-
 pluginManagement {
-    val flutterSdkPath: String by lazy {
-        val properties = Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val path = properties.getProperty("flutter.sdk")
-        checkNotNull(path) { "flutter.sdk not set in local.properties" }
-    }
+    val flutterSdkPath: String = providers.gradleProperty("flutter.sdk")
+        .orElse(
+            file("local.properties").let { localPropsFile ->
+                val props = java.util.Properties()
+                localPropsFile.inputStream().use { props.load(it) }
+                props.getProperty("flutter.sdk") ?: throw GradleException("flutter.sdk not set in local.properties")
+            }
+        ).get()
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -19,8 +19,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 include(":app")
